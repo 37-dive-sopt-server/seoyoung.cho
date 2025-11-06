65b1532cba7796661b03f19bae0c2085a55b444d
Seoyoung Cho <bera1125@naver.com>
2025-11-06 17:28:03 +0900

    Feat: N+1 문제 해결을 위해 @EntityGraph 사용

--- src/main/java/org/sopt/article/repository/ArticleRepository.java
+++ src/main/java/org/sopt/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package org.sopt.article.repository;
 
 import org.sopt.article.domain.Article;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,10 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByTitleContaining(String title);
 
     List<Article> findByMemberNameContaining(String memberName);
+
+    // N+1 해결을 위한 findAll() 오버라이드
+    @EntityGraph(attributePaths = {"member"})
+    @Override
+    List<Article> findAll();
 }
 
