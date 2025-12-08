9a2c22b8979ca070a51cfeaac0b1be98407ad015
ssyoung02 <bera1125@naver.com>
2025-12-09 01:11:36 +0900

    Feat: N+1 문제 해결 및 댓글 수 카운트 최적화

--- src/main/java/org/sopt/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/sopt/domain/comment/repository/CommentRepository.java
@@ -1,12 +1,16 @@
 package org.sopt.domain.comment.repository;
 
 import org.sopt.domain.comment.domain.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 모든 댓글 조회 (생성 오름차순)
+    @EntityGraph(attributePaths = {"member", "article"})
     List<Comment> findByArticleIdOrderByCreatedAtAsc(Long articleId);
 
+    // 특정 게시글의 댓글 개수 조회 (카운트 쿼리만 실행)
+    long countByArticleId(Long articleId);
 }
